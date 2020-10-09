cfc1b04e64f755527ce262175ac0100dac4b934a
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-10-09 07:31:24 +0200

    ui: make appbar title app name

--- app/src/main/java/org/mrlem/placetime/ui/MainActivity.kt
+++ app/src/main/java/org/mrlem/placetime/ui/MainActivity.kt
@@ -3,8 +3,6 @@ package org.mrlem.placetime.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import org.mrlem.placetime.R
@@ -15,14 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // setup tabs
-        val navController = findNavController(R.id.navHostFragment)
-        val appBarConfiguration = AppBarConfiguration(tabIds)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    companion object {
-        private val tabIds = setOf(R.id.navigation_map, R.id.navigation_dashboard, R.id.navigation_log)
+        navView.setupWithNavController(findNavController(R.id.navHostFragment))
     }
 }
