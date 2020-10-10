6cbd13f14d9bf907b692960c9aa9b5dfd91f08f6
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-10-10 09:25:29 +0200

    ui: populate map with places

---
+++ app/src/main/java/org/mrlem/placetime/ui/map/MapAdapter.kt
@@ -0,0 +1,35 @@
+package org.mrlem.placetime.ui.map
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import org.mrlem.placetime.core.model.Place
+
+class MapAdapter(private val map: GoogleMap) {
+
+    private var bounds: LatLngBounds? = null
+
+    fun updatePlaces(places: List<Place>) {
+        val boundsBuider = LatLngBounds.builder()
+        map.clear()
+
+        // TODO - only diff
+        places.forEach { place ->
+            val location = LatLng(place.latitude, place.longitude)
+            boundsBuider.include(location)
+            map.addMarker(MarkerOptions().position(location).title(place.label))
+        }
+
+        bounds = boundsBuider
+            .takeIf { places.isNotEmpty() }
+            ?.build()
+    }
+
+    fun center() {
+        bounds?.let {
+            map.moveCamera(CameraUpdateFactory.newLatLngBounds(it, 150))
+        }
+    }
+}
--- app/src/main/java/org/mrlem/placetime/ui/map/MapFragment.kt
+++ app/src/main/java/org/mrlem/placetime/ui/map/MapFragment.kt
@@ -6,11 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.mrlem.placetime.R
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private val viewModel by lazy { ViewModelProvider(this).get(MapViewModel::class.java) }
+    private lateinit var mapAdapter: MapAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_map, container, false)
@@ -18,8 +25,19 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO - init observations
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+   }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mapAdapter = MapAdapter(googleMap)
+
+        // observations
         viewModel.places
-            .observe(viewLifecycleOwner) { places -> println("loaded places = $places") }
+            .observe(viewLifecycleOwner) { places ->
+                mapAdapter.updatePlaces(places)
+                mapAdapter.center()
+            }
     }
 }
