ac66d197b36135d87a5ebd659ef646306483334b
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-10-13 22:37:36 +0200

    cleanup: removed hardcoded string

--- app/src/main/java/org/mrlem/placetime/ui/log/ViewHolder.kt
+++ app/src/main/java/org/mrlem/placetime/ui/log/ViewHolder.kt
@@ -3,6 +3,7 @@ package org.mrlem.placetime.ui.log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_event.view.*
+import org.mrlem.placetime.R
 import org.mrlem.placetime.core.domain.model.EventAndPlace
 import java.text.SimpleDateFormat
 import java.util.*
@@ -10,8 +11,8 @@ import java.util.*
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(event: EventAndPlace) {
-        // TODO - improve that
-        itemView.description.text = "${event.place.label} ${event.event.type}"
+        val res = itemView.context.resources
+        itemView.description.text = res.getString(R.string.log_format, event.place.label, event.event.type)
         itemView.time.text = formatter.format(Date(event.event.time))
     }
 
